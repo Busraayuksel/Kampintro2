f33bb3c148afa45c5bbf41c9f98b245a410a8a28
büşra yüksel <busraa.yksel@outlook.de>
2021-02-02 22:07:22 +0100

    Add project files.

---
+++ Kampintro2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Kampintro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //type safety - tip güvenliği
+            //Do not repeat yoursef  - kendini tekrarlama 
+            //kategoriEtiketi bir değer tutucu, alias
+
+            string kategoriEtiketi = "kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun) 
+            {
+                Console.WriteLine("Artış butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
